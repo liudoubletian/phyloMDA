22f2f97351d27aaf2058d034782f634e5e7b6d55
liudoubletian <52096751+liudoubletian@users.noreply.github.com>
2022-04-17 00:05:07 +0800

    Add files via upload

---
+++ requirement.R
@@ -0,0 +1,19 @@
+#####R libraries#####
+install.packages(MGLM);library(MGLM)
+install.packages(plyr);library(plyr)
+install.packages(caper);library(caper)
+install.packages(genlasso);library(genlasso)
+install.packages(magrittr);library(magrittr)
+install.packages(foreach);library(foreach)
+install.packages(ape);library(ape)
+install.packages(miLineage);library(miLineage)
+install.packages(ggplot2);library(ggplot2)
+install.packages(dplyr);library(dplyr)
+install.packages(readxl);library(readxl)
+library(methods)
+
+
+install.packages("BiocManager");library(BiocManager)
+BiocManager::install("phyloseq");library(phyloseq)
+BiocManager::install("ggtree");library(ggtree)
+devtools::install_github("ZRChao/adaANCOM");library(adaANCOM)
